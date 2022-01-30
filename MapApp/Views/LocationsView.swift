5ca13529c5e966e7853b0fee0594f42321e864a0
Kuncans <akafauna@gmail.com>
2022-01-30 01:50:48 +0000

    Updated map region and location on viewmodel init

---
+++ MapApp/Views/LocationsView.swift
@@ -0,0 +1,28 @@
+//
+//  LocationsView.swift
+//  MapApp
+//
+//  Created by Duncan Kent on 30/01/2022.
+//
+
+import SwiftUI
+import MapKit
+
+struct LocationsView: View {
+    
+    @EnvironmentObject private var vm: LocationsViewModel
+    
+    var body: some View {
+        ZStack {
+            Map(coordinateRegion: $vm.mapRegion)
+                .ignoresSafeArea()
+        }
+    }
+}
+
+struct LocationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationsView()
+            .environmentObject(LocationsViewModel())
+    }
+}
