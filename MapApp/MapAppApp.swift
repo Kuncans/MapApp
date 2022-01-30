da4715e2c06c7925c5e1c3e326eff7452109f138
Kuncans <akafauna@gmail.com>
2022-01-30 00:44:55 +0000

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Duncan Kent on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
